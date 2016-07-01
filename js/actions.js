2e7cab9e7d873e37fdc28e553f45111bd81c0981
Andre <elij.richards22@gmail.com>
2016-07-01 03:56:40 -0400

    added case of no ingredients

--- js/actions.js
+++ js/actions.js
@@ -2,7 +2,8 @@ var startBtn = $('#startBtn');
 var currQuestion = $('.currentQuestion');
 var prefWrap = $('.prefContainer');
 var drinkWrap = $('.drinkContainer');
-var preflist = $('.preferList ul');
+var prefList = $('.preferList ul');
+var drinkList = $('.drinkIngreds ul');
 var moreBtn = $('#moreBtn');
 
 // Bartender Prototype //
@@ -24,7 +25,7 @@ var Pantry = function(ingredients) {
 var ingredientArray = [
     ['Strong', 'Douse of Rum', 'Splash of Gin', 'Shot of Whiskey'],
     ['Salty', 'Olive on a Stick', 'Salt-Dusted Rim', 'Whisper of Saline'],
-    ['Bitter', 'Shake of bitters', 'Dose of Tonic', 'Twist of Lemon Peel'],
+    ['Bitter', 'Shake of Bitters', 'Dose of Tonic', 'Twist of Lemon Peel'],
     ['Sweet', 'Sugar Cube', 'Spoonful of Honey', 'Half-pint of Cola'],
     ['Fruity', 'Slice of Orange', 'Creme de Cassis', 'Cherry on Top']
 ];
@@ -49,24 +50,28 @@ function createDrink() {
     var drinkQuestions = new Bartender(questionArray);
     var ingredItems = new Pantry(ingredientArray);
     var counter = 0;
-    currQuestion.html(drinkQuestions.questions[counter]); // Load first question
+    var changeQuestion = currQuestion.html(drinkQuestions.questions[counter]);
+    changeQuestion; // Load first question
 
     $('input').on('click', function() {
         if (this.id === 'yesBtn' && counter < questionArray.length) {
             var rndmIdx = Math.floor((Math.random() * 3) + 1);
-            preflist.append('<li>' + ingredItems.ingredients[counter][0] + '</li>'); // Add user's drink preference to UI list
-            $('.drinkIngreds ul').append('<li>' + ingredItems.ingredients[counter][rndmIdx] + '</li>'); // Add ingredients to UI drink list
+            prefList.append('<li>' + ingredItems.ingredients[counter][0] + '</li>'); // Add user's drink preference to UI list
+            drinkList.append('<li>' + ingredItems.ingredients[counter][rndmIdx] + '</li>'); // Add ingredients to UI drink list
         } else if (this.id === "noBtn" && counter < questionArray.length) {
-            preflist.append('<li>Not ' + ingredItems.ingredients[counter][0] + '</li>'); // Add user's drink preference to UI list
+            prefList.append('<li>Not ' + ingredItems.ingredients[counter][0] + '</li>'); // Add user's drink preference to UI list
         }
         counter++;
-        currQuestion.html(drinkQuestions.questions[counter]);
+        changeQuestion;
         if (counter == 5) {
             drinkWrap.css('display', 'block'); // Shows drink container and more button upon last question
             moreBtn.css('display', 'block');
+            if ($('.drinkIngreds ul li').length == 0){
+                drinkList.append('<li>I\'ll just get you a water</li>');
+            }
             moreBtn.on('click', function() {
                 counter = 0; // Reset variables
-                currQuestion.html(drinkQuestions.questions[counter]);
+                changeQuestion;
                 anotherDrink();
                 drinkWrap.css('display', 'none');
                 moreBtn.css('display', 'none');
